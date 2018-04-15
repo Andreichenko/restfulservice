26239f390ed6e53f0bd30b7dd73ce2298a73ce25
aleksandrandreichenko <rsn_alex@mail.ru>
2018-04-16 00:17:18 +0300

    Добавил тест для данных из api, тест работает с ошибкой

---
+++ src/test/java/com/frei/zabbtest/test/domain/SuggestionTest.java
@@ -0,0 +1,70 @@
+package com.frei.zabbtest.test.domain;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.frei.zabbtest.domain.GeoPositon;
+import com.frei.zabbtest.domain.Suggestion;
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ *
+ *  T E S T
+ *
+ *  For Suggestion class
+ *
+ *
+ * by Andreichenko
+ */
+
+public class SuggestionTest {
+
+    @Test
+    public void testJsonFormat() throws Exception{
+
+        //prepare
+        String source = "{" +
+                "\"_id\": 376217," +
+                "\"key\": \"ber-key\"," +
+                "\"name\": \"Berlin\"," +
+                "\"fullName\": \"Berlin, Germany\"," +
+                "\"iata_airport_code\": null," +
+                "\"type\": \"location\"," +
+                "\"country\": \"Germany\"," +
+                "\"geo_position\": {\"latitude\": 52.52437,\"longitude\": 13.41053}," +
+                "\"locationId\": 8384," +
+                "\"inEurope\": true," +
+                "\"countryId\": 56," +
+                "\"countryCode\": \"DE\"," +
+                "\"coreCountry\": true," +
+                "\"distance\": 12538," +
+                "\"names\": {\"pt\": \"Berlim\",\"ru\": \"Берлин\",\"it\": \"Berlino\"}," +
+                "\"alternativeNames\": {\"is\": \"Berlín\",\"fi\": \"Berliini\",\"zh\": \"柏林\"}" +
+                "}";
+        Suggestion expected = Suggestion.builder()
+                .id(376217)
+                .key("ber-key")
+                .name("Berlin")
+                .fullName("Berlin, Germany")
+                .iataAirportCode(null)
+                .type("location")
+                .country("Germany")
+                .geoPositon(new GeoPositon(52.52437, 13.41053))
+                .locationId(8384)
+                .inEurope(true)
+                .countryId(56)
+                .countryCode("DE")
+                .coreCountry(true)
+                .distance(12538)
+                .names(ImmutableMap.of("pt", "Berlim", "ru", "Берлин", "it", "Berlino"))
+                .alternativeNames(ImmutableMap.of("is", "Berlín", "fi", "Berliini", "zh", "柏林"))
+                .build();
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        assertThat(mapper.readValue(source, Suggestion.class), equalTo(expected));
+    }
+}
